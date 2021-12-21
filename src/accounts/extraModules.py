ce333e4d5a4d6e7dc211b885d584eaf2eebc7322
Dipan Bhusal <levilins40@gmail.com>
2021-12-21 19:45:26 +0545

    Created sha hash function

--- src/accounts/extraModules.py
+++ src/accounts/extraModules.py
@@ -1,9 +1,24 @@
-def prepareKeys(public, private):
+from hashlib import sha256
+import base64
+
+def prepareKeys(n_e_d):
     """
         Returns list of [n. e] as public key and [n, d] as private key from format 'n-e'
     """
-    public = public.split('-')
-    public = [int(key)/5 for key in public] # [n, e]
-    private = private.split('-')
-    private = [int(key)/5 for key in private] #[n, d]
-    return public, private
+    try:
+        keys = n_e_d.split('-')
+    except TypeError:
+        keys = n_e_d.decode('utf-8').split('-')
+    public_key = [int(keys[0]), int(keys[1])]
+    private_key = [int(keys[0]), int(keys[2])]
+
+    return public_key, private_key
+
+
+def getSHA256Hash(text):
+    """
+    Takes plaintext as input and returns hash value
+    """
+    cipher=sha256(bytes(text, 'utf-8')).digest()
+    cipher=base64.b64encode(cipher) # encoding sha hash in base64 required
+    return cipher
