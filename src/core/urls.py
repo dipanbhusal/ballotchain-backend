48427510a62c93c9e7f68aeb577bb63189503f6e
Dipan Bhusal <levilins40@gmail.com>
2022-01-27 21:41:46 +0545

    Minor changes

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls'))
+    path('api/accounts/', include('accounts.urls')),
+    path('api/election/', include('election.urls')),
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
