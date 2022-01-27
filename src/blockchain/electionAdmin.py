b5f908b3032f5b09aedb65ceabebd8660981c928
Dipan Bhusal <levilins40@gmail.com>
2022-01-27 21:43:03 +0545

    Added start election and remote voter function

--- src/blockchain/electionAdmin.py
+++ src/blockchain/electionAdmin.py
@@ -33,6 +33,25 @@ class ElectionAdmin:
         receipt = self.w3.eth.waitForTransactionReceipt(txn_hash)
 
         return dict(receipt)
+    
+    def remove_voter(self, address):
+        """
+        Removees voter if it exists.
+        input: voter's address
+        """
+        init_txn = self.contract.functions\
+            .removeVoter(address).buildTransaction({
+                'from': self.account.address,
+                'nonce': self.w3.eth.getTransactionCount(self.account.address),
+                'gas': 100000,
+                'gasPrice': self.w3.eth.gasPrice
+            })
+
+        signed = self.account.signTransaction(init_txn)
+        txn_hash = self.w3.eth.send_raw_transaction(signed.rawTransaction)
+        recipt = self.w3.eth.waitForTransactionReceipt(txn_hash)
+        return dict(recipt)
+
 
     def add_candidate(self, address):
         """
@@ -52,3 +71,38 @@ class ElectionAdmin:
         receipt = self.w3.eth.waitForTransactionReceipt(txn_hash)
 
         return dict(receipt)
+
+    def start_election(self):
+        """
+        Module for starting election
+        """
+        init_txn = self.contract.functions\
+            .startElection().buildTransaction({
+                'from': self.account.address,
+                'nonce': self.w3.eth.getTransactionCount(self.account.address),
+                'gas': 100000,
+                'gasPrice': self.w3.eth.gasPrice
+            })
+        
+        signed = self.account.signTransaction(init_txn)
+        txn_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
+        recipt = self.w3.eth.waitForTransactionReceipt(txn_hash)
+
+        return dict(recipt)
+
+    def end_election(self):
+        """
+        Module for ending election after it is started
+        """
+        init_txn = self.contract.functions\
+            .endElection().buildTransaction({
+                'from': self.account.address,
+                'nonce': self.w3.eth.getTransactionCount(self.account.address),
+                'gas': 100000,
+                'gasPrice': self.w3.eth.gasPrice
+            })
+        signed = self.account.signTransaction(init_txn)
+        txn_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
+        recipt = self.w3.eth.waitForTransactionReceipt(txn_hash)
+
+        return dict(recipt)
