b7ef0279274294ff8ab4d3bf9b92ce4fd85a9e32
Dipan Bhusal <levilins40@gmail.com>
2022-01-28 21:45:57 +0545

    Added icon

--- src/blockchain/contractConfig.py
+++ src/blockchain/contractConfig.py
@@ -6,7 +6,7 @@ from .authenticate import w3
 with open('/Users/xxxx/dpn/7th sem/project work/online-voting-system-backend/src/blockchain/contract.json') as f:
     contract_iface = json.load(f)
 
-contract_address = "0x8beD8DAbd3775441003232E4BBDE2B21c9884efD"
+contract_address = "0x5aE08F85e970f3b63dE8889dA7979CE19e5f37c0"
 
 contract = w3.eth.contract(address=contract_address,
     abi=contract_iface['abi']
