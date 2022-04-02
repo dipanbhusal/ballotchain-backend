b332883a75517303adb9859b1b74c7181e61c237
Dipan Bhusal <levilins40@gmail.com>
2022-04-02 12:27:25 +0545

    minor

--- src/blockchain/contractConfig.py
+++ src/blockchain/contractConfig.py
@@ -2,12 +2,12 @@ import json
 import os
 
 from .authenticate import w3
-with open('/home/dipan/project/online-voting-system-backend/src/blockchain/contract.json') as f:
-# with open('/Users/xxxx/dpn/7th sem/project work/online-voting-system-backend/src/blockchain/contract.json') as f:
+# with open('/home/dipan/project/online-voting-system-backend/src/blockchain/contract.json') as f:
+with open('/Users/xxxx/dpn/7th sem/project work/online-voting-system-backend/src/blockchain/contract.json') as f:
 
     contract_iface = json.load(f)
 
-contract_address = "0xabDb6397D1ef9b60122C67D30990DD5f7cF0682C"
+contract_address = "0xD7935367885406C1F9c89d7425E00FB2E11c687f"
 
 contract = w3.eth.contract(address=contract_address,
     abi=contract_iface['abi']
